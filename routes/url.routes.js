e51ded5175d7cbadc8c8576d24ed65587f464589
solomonade1 <solomonadegboyega1@gmail.com>
2023-11-21 14:48:49 +0100

    fix: remove session limiter for unregister users

--- routes/url.routes.js
+++ routes/url.routes.js
@@ -18,14 +18,14 @@ import { loginLimiter, sessionLimiter } from "../utils/limiter.js";
 const urlRouter = express.Router();
 
 urlRouter.post("/url", verifyToken, loginLimiter, createUrl);
-urlRouter.post("/url/unregister", sessionLimiter, unRegisterUrl);
+urlRouter.post("/url/unregister", unRegisterUrl);
 urlRouter.get("/url/all", verifyToken, getAllUlr);
 //urlRouter.get("/:urlId", redirectUrl)
 urlRouter.get("/:urlId", shortenUrl);
 urlRouter.get("/url/single/:id", verifyToken, getSingleUrl);
 urlRouter.get("/url/users", verifyToken, getUserUrls);
 urlRouter.get("/url/infos/:id", verifyToken, getUserUrlsClickInfo);
-urlRouter.get("/url/users/unregister", sessionLimiter, getUnRegisterUserUrls);
+urlRouter.get("/url/users/unregister",  getUnRegisterUserUrls);
 urlRouter.put("/url/update/:id", verifyToken, updateUrl);
 urlRouter.put("/url/updatenow/:id", verifyToken, updateUrlNow);
 urlRouter.delete("/url/delete/:id", verifyToken, deleteUrlById);
